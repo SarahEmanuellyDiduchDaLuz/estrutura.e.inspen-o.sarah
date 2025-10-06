b721db8e67294d0c796e6e6e5054942d9438257c
SarahEmanuellyDiduchDaLuz <diduch.luz@escola.pr.gov.br>
2025-10-06 21:42:12 +0000

    TERMINO

--- script.js
+++ script.js
@@ -2,22 +2,37 @@ const taskForm = document.getElementById('taskForm');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 
+// Função que cria um item da lista
 function createTaskItem(text) {
-    const li= document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = text;
-    const deteleBtn = document.createElement('button');
-    deteleBtn.textContent = 'Remover';
-    deteleBtn.className = 'delete-btn';
-    deteleBtn.type = 'button';
-    li.appendChild(span);
-    li.appendChild(deteleBtn);
-    return li;
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.textContent = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Remover';
+  deleteBtn.className = 'delete-btn';
+  deleteBtn.type = 'button';
+
+  // Quando clicar no botão "Remover", apaga o item
+  deleteBtn.addEventListener('click', () => {
+    li.remove();
+  });
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+  return li;
 }
 
-taskForm.addEventListener('submit', function(e) {
+taskForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  const text = taskInput.ariaValueMax.trim();
-  if (!text) return;
-  const li = 
-}
+
+  const text = taskInput.value.trim();
+
+  if (!text) return; 
+
+  const li = createTaskItem(text);
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+  taskInput.focus();
+});
